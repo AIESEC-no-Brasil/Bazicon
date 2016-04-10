9364b58cd353fdaedf50d5ba1a2304b9c527d906
ivsucram <ivsucram@gmail.com>
2016-04-10 10:56:10 -0300

    Agora vai mlk

---
+++ config/initializers/cookies_serializer.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+# TODO check what this file do
+Rails.application.config.action_dispatch.cookies_serializer = :json
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Myapp::Application.config.session_store :cookie_store, key: '_myapp_session'
+Myapp::Application.config.session_store :cookie_store, key: '_bazicon_session'
