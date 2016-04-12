43408deb8dfb2b9c83fe8dd42847153c352dba4b
ivsucram <ivsucram@gmail.com>
2016-04-12 02:18:22 -0300

    Form 2 de Digital Transformation

--- app/controllers/digital_transformation_controller.rb
+++ app/controllers/digital_transformation_controller.rb
@@ -6,7 +6,7 @@ class DigitalTransformationController < ApplicationController
 
   # POST /dt/difficulties_success
   def difficulties_success
-    userName = params['userName']
+    user_name = params['userName']
     password = params['password']
     difficulty = params['difficulties']
 
@@ -16,7 +16,7 @@ class DigitalTransformationController < ApplicationController
       expa = EXPA.client
     end
 
-    expa.auth(userName,password)
+    expa.auth(user_name,password)
     if expa.get_token.nil?
       flash['text-danger'] = 'Sua conta não foi encontrada. Verifique se você digitou seu e-mail e senha corretamente'
       return redirect_to digital_transformation_difficulties_path
@@ -30,10 +30,10 @@ class DigitalTransformationController < ApplicationController
       end
 
       json = if user.customized_fields.nil?
-        user.customized_fields = {}
-      else
-        JSON.parse(user.customized_fields)
-      end
+               user.customized_fields = {}
+             else
+               JSON.parse(user.customized_fields)
+             end
 
       hash_difficulty = {}
       hash_difficulty['field_name'] = 'O que te impediria de fazer um intercâmbio nas próximas férias?'
@@ -50,12 +50,60 @@ class DigitalTransformationController < ApplicationController
 
   # GET /dt/prevents
   def prevents
-
+    prevents_options
+    render layout: "empty"
   end
 
   # POST /dt/prevents_success
   def prevents_success
+    user_name = params['userName']
+    password = params['password']
+    prevent_option = params['preventOption']
+    prevent_detail = params['preventDetail']
 
+    if EXPA.client.nil?
+      expa = EXPA.setup()
+    else
+      expa = EXPA.client
+    end
+
+    expa.auth(user_name,password)
+    if expa.get_token.nil?
+      flash['text-danger'] = 'Sua conta não foi encontrada. Verifique se você digitou seu e-mail e senha corretamente'
+      return redirect_to digital_transformation_prevents_path
+    else
+      current_person = EXPA::CurrentPerson.get_current_person
+      user = ExpaPerson.find_by_xp_id(current_person.id)
+      if user.nil?
+        user = ExpaPerson.new
+        user.update_from_expa(current_person)
+        user.save
+      end
+
+      prevents_options
+
+      json = if user.customized_fields.nil?
+               user.customized_fields = {}
+             else
+               JSON.parse(user.customized_fields)
+             end
+
+      hash_prevent = {}
+      hash_prevent['field_name'] = 'O que te impediria de fazer um intercâmbio nas próximas férias?'
+      hash_prevent['type'] = 'text'
+      if prevent_option == @options.keys[-1]
+        hash_prevent['value'] = prevent_detail
+      else
+        hash_prevent['value'] = @options[prevent_option]
+      end
+
+      json['prevents'] = hash_prevent
+
+      user.customized_fields = json.to_json.to_s
+      user.save
+      @user = user
+    end
+    render layout: "empty"
   end
 
   # GET /expa/sign_up
@@ -66,5 +114,18 @@ class DigitalTransformationController < ApplicationController
   # POST /expa/sign_up
   def expa_sign_up_success
 
+  end
+
+  private
+
+  def prevents_options
+    @options = {}
+    @options['value1'] = 'Não tenho dinheiro'
+    @options['value2'] = 'Meus pais não deixam'
+    @options['value3'] = 'Minhas férias são muito curtas'
+    @options['value4'] = 'Não sei falar outro idioma'
+    @options['value5'] = 'Não encontro vagas no país de minha preferência'
+    @options['value6'] = 'Nunca viajei sozinho(a)'
+    @options['other'] = 'Outro'
   end
 end
