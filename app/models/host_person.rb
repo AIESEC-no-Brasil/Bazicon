46a10e32cd1ed4d6cb76f8e5d787db33a685ea98
Mauro Victor <mauro.victor5@gmail.com>
2016-04-22 18:03:33 -0300

    rebuilding host tests

--- app/models/host_person.rb
+++ app/models/host_person.rb
@@ -1,2 +1,7 @@
 class HostPerson < ActiveRecord::Base
+	def self.list_free
+		free = HostPerson.where.not("tmp_responsable_id=? 
+									AND date_approach=?
+									AND date_alignment_meeting=?", nil, nil, nil)
+	end
 end
--- test/models/host_person_test.rb
+++ test/models/host_person_test.rb
@@ -1,7 +1,42 @@
-require 'test_helper'
+require 'minitest/autorun'
 
 class HostPersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  end
+
+  def teardown
+  	HostPerson.all.each do |host|
+      host.destroy
+    end
+  end
+
+  def test_save
+  	person = HostPerson.new
+  	person.full_name = "Fulano de tal"
+  	person.phone = 91239
+  	person.email = "mail.@mail.com"
+  	person.address = "12 stree, 180"
+  	assert person.save, "Não Salvou"
+  end
+
+  def test_delete
+  	
+  	HostPerson.all.each do |host|
+      host.destroy
+    end
+
+  	person = HostPerson.new
+  	person.full_name = "Fulano de tal"
+  	person.phone = 91239
+  	person.email = "mail.@mail.com"
+  	person.address = "12 stree, 180"
+  	person.save
+  	assert person.destroy, "Não deletou"
+  end
+
+  def test_list_all_free
+    hosts = HostPerson.list_free
+    assert(hosts.length == 0, "List Free Assert 1) Wrong number of hosts. It was supposed to be 0, it got #{hosts.count} instead")
+  end
+
 end
