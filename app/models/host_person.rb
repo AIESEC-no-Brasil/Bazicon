c528b0098f5b562eb24a7324e18008f511b586cc
Mauro Victor <mauro.victor5@gmail.com>
2016-04-23 20:25:23 -0300

    free_host tests done

--- app/models/host_person.rb
+++ app/models/host_person.rb
@@ -1,7 +1,9 @@
 class HostPerson < ActiveRecord::Base
 	def self.list_free
 		free = HostPerson.where.not("tmp_responsable_id=? 
+									AND tmp_who_realized_meeting_id=?
 									AND date_approach=?
-									AND date_alignment_meeting=?", nil, nil, nil)
+									AND date_alignment_meeting=?
+									AND date_alignment_meeting>=?", nil, nil, nil, nil, Time.now)
 	end
 end
--- test/models/host_person_test.rb
+++ test/models/host_person_test.rb
@@ -35,8 +35,128 @@ class HostPersonTest < ActiveSupport::TestCase
   end
 
   def test_list_all_free
+  	HostPerson.all.each do |host|
+      host.destroy
+    end
+
     hosts = HostPerson.list_free
     assert(hosts.length == 0, "List Free Assert 1) Wrong number of hosts. It was supposed to be 0, it got #{hosts.count} instead")
+
+    (1..10).each do |i|
+	  	person = HostPerson.new
+	  	person.full_name = "Lead #{i}"
+	  	person.phone = 91239
+	  	person.email = "mail.@mail.com"
+	  	person.address = "12 stree, 18#{i}"
+	  	person.save
+	end 
+
+	(1..10).each do |i|
+		person = HostPerson.new
+	  	person.full_name = "Lead #{i}"
+	  	person.phone = 91239
+	  	person.email = "mail.@mail.com"
+	  	person.address = "12 stree, 18#{i}"
+	  	person.tmp_responsable_id = 78789
+    	person.tmp_who_realized_meeting_id = 79887
+    	person.date_approach = Time.new(2016,4,10)
+    	person.date_alignment_meeting = 78789
+	  	person.save
+	end
+
+    hosts = HostPerson.list_free
+    assert(hosts.length == 0, "List Free Assert 2) Wrong number of hosts. It was supposed to be 0, it got #{hosts.count} instead")
+
+ 	person = HostPerson.new
+  	person.full_name = "Lead Com reunião Por Fazer"
+  	person.phone = 91239
+  	person.email = "mail.@mail.com"
+  	person.address = "12 stree, 180"
+  	person.tmp_responsable_id = 78789
+	person.tmp_who_realized_meeting_id = 79887
+	person.date_approach = Time.new(2016,4,10)
+	person.date_alignment_meeting = 78789
+	date_alignment_meeting = Time.new(Time.now.year,Time.now.month, Time.now.day+2)
+  	person.save
+  	
+    hosts = HostPerson.list_free
+    assert(hosts.length == 0, "List Free Assert 3) Wrong number of hosts. It was supposed to be 0, it got #{hosts.count} instead")
+
+ 	(1..5).each do |i|
+	 	person = HostPerson.new
+	  	person.full_name = "Free #{i}"
+	  	person.phone = 91239
+	  	person.email = "mail.@mail.com"
+	  	person.address = "12 stree, 180"
+	  	person.tmp_responsable_id = 78789
+		person.tmp_who_realized_meeting_id = 79887
+		person.date_approach = Time.new(2016,4,10)
+		person.date_alignment_meeting = 78789
+		person.date_alignment_meeting = Time.new(Time.now.year,Time.now.month, Time.now.day-2)
+	  	person.save
+	end
+
+    hosts = HostPerson.list_free
+    assert(hosts.length == 5, "List Free Assert 4) Wrong number of hosts. It was supposed to be 5, it got #{hosts.count} instead")
+
+    HostPerson.all.each do |host|
+      host.destroy
+    end
+
+    (1..3).each do |i|
+	    person = HostPerson.new
+	  	person.full_name = "Lead Com reunião Por Fazer #{i}"
+	  	person.phone = 91239
+	  	person.email = "mail.@mail.com"
+	  	person.address = "12 stree, 180"
+	  	person.tmp_responsable_id = 78789
+		person.tmp_who_realized_meeting_id = 79887
+		person.date_approach = Time.new(2016,4,10)
+		person.date_alignment_meeting = 78789
+		date_alignment_meeting = Time.new(Time.now.year,Time.now.month, Time.now.day+2)
+	  	person.save
+  	end
+
+  	(1..3).each do |i|
+		person = HostPerson.new
+	  	person.full_name = "Lead #{i}"
+	  	person.phone = 91239
+	  	person.email = "mail.@mail.com"
+	  	person.address = "12 stree, 18#{i}"
+	  	person.tmp_responsable_id = 78789
+    	person.tmp_who_realized_meeting_id = 79887
+    	person.date_approach = Time.new(2016,4,10)
+    	person.date_alignment_meeting = 78789
+	  	person.save
+	end
+
+	(1..5).each do |i|
+	  	person = HostPerson.new
+	  	person.full_name = "Lead #{i}"
+	  	person.phone = 91239
+	  	person.email = "mail.@mail.com"
+	  	person.address = "12 stree, 18#{i}"
+	  	person.save
+	end
+
+	(1..3).each do |i|
+	 	person = HostPerson.new
+	  	person.full_name = "Free #{i}"
+	  	person.phone = 91239
+	  	person.email = "mail.@mail.com"
+	  	person.address = "12 stree, 180"
+	  	person.tmp_responsable_id = 78789
+		person.tmp_who_realized_meeting_id = 79887
+		person.date_approach = Time.new(2016,4,10)
+		person.date_alignment_meeting = 78789
+		person.date_alignment_meeting = Time.new(Time.now.year,Time.now.month, Time.now.day-2)
+	  	person.save
+	end
+
+	hosts = HostPerson.list_free
+    assert(hosts.length == 3, "List Free Assert 5) Wrong number of hosts. It was supposed to be >>3<<, it got >>#{hosts.count}<< instead")
+
+
   end
 
 end
