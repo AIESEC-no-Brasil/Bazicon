feaa444c71eb869c78ef01f92ad757b15333924e
Bianca <biaseemann@gmail.com>
2016-05-03 11:25:07 -0300

    mostrando preview dos documentos pela extensão do chrome, não mais pelo dropbox

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -8,22 +8,22 @@ class Archive < ActiveRecord::Base
   has_many :tags, through: :archive_tags, class_name: 'Tag'
 
   def get_file_type
-    if self.archive_extension== 'jpg' || self.archive_extension =='jpeg'  || self.archive_extension =='png' || self.archive_extension =='jpeg' ||
-        self.archive_extension =='bmp' || self.archive_extension =='gif' || self.archive_extension =='tif'
+    if self.archive_extension== '.jpg' || self.archive_extension =='.jpeg'  || self.archive_extension =='.png' || self.archive_extension =='.jpeg' ||
+        self.archive_extension =='.bmp' || self.archive_extension =='.gif' || self.archive_extension =='.tif'
       return Archive.type_of_files[:image]
-    elsif self.archive_extension == 'pdf'
+    elsif self.archive_extension == '.pdf'
       return Archive.type_of_files[:pdf]
-    elsif self.archive_extension == 'doc' || self.archive_extension == 'docx'
+    elsif self.archive_extension == '.doc' || self.archive_extension == '.docx'
       return Archive.type_of_files[:word]
-    elsif self.archive_extension == 'ppt' || self.archive_extension == 'pptx' || self.archive_extension == 'pptm'
+    elsif self.archive_extension == '.ppt' || self.archive_extension == '.pptx' || self.archive_extension == '.pptm'
       return Archive.type_of_files[:ppt]
-    elsif self.archive_extension == 'xls' || self.archive_extension == 'xlsx' || self.archive_extension == 'xlsm'
+    elsif self.archive_extension == '.xls' || self.archive_extension == '.xlsx' || self.archive_extension == '.xlsm'
       return Archive.type_of_files[:excel]
-    elsif self.archive_extension == 'mp3' || self.archive_extension == 'wav' || self.archive_extension == '3gp' || self.archive_extension == 'wma'||
-       self.archive_extension == 'm4a'||self.archive_extension == 'flac'
+    elsif self.archive_extension == '.mp3' || self.archive_extension == '.wav' || self.archive_extension == '.3gp' || self.archive_extension == '.wma'||
+       self.archive_extension == '.m4a'||self.archive_extension == '.flac'
       return Archive.type_of_files[:music]
-    elsif self.archive_extension == 'mp4' || self.archive_extension == 'avi' || self.archive_extension == 'wmv' || self.archive_extension == 'mpg'||
-        self.archive_extension == 'mpeg'||self.archive_extension == 'flv' ||self.archive_extension == 'mkv'
+    elsif self.archive_extension == '.mp4' || self.archive_extension == '.avi' || self.archive_extension == '.wmv' || self.archive_extension == '.mpg'||
+        self.archive_extension == '.mpeg'||self.archive_extension == '.flv' ||self.archive_extension == '.mkv'
       return Archive.type_of_files[:video]
     end
     return Archive.type_of_files[:other]
