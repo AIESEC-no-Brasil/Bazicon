3b5a0252ed827f00e9130326e281b939b432ee0b
luancorumba <luan@corumba.net>
2016-10-31 11:06:23 -0200

    simple_token evolution

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -3,11 +3,15 @@ class TokenController < ApplicationController
 		if EXPA.client.nil?
 			xp = EXPA.setup()
 			xp.auth(ENV['SIMPLE_TOKEN_EMAIL'],ENV['SIMPLE_TOKEN_PASSWORD'])
-			puts 'here'
-			render :json => xp.get_updated_token
+			response.headers['Access-Control-Allow-Credentials'] = 'true'
+			response.headers['Access-Control-Allow-Origin'] = '*'
+			response.headers['Access-Control-Max-Age'] = '1728000'
+			render :json => {:token => xp.get_updated_token.to_s}
 		else
-			puts 'Not nil'
-			render :json => EXPA.client.get_updated_token
+			response.headers['Access-Control-Allow-Credentials'] = 'true'
+			response.headers['Access-Control-Allow-Origin'] = '*'
+			response.headers['Access-Control-Max-Age'] = '1728000'
+			render :json => {:token => EXPA.client.get_updated_token.to_s}
 		end
 	end
 end
