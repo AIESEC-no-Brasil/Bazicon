98b1d6b16cf785f2639256bf5feb0dbdf2afb8ff
luancorumba <luan@corumba.net>
2016-11-10 17:37:24 -0200

    loging token requests

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -1,16 +1,19 @@
 class TokenController < ApplicationController
 	def get_analytics_token
+		puts Thread.current.object_id
 		if EXPA.client.nil?
 			xp = EXPA.setup()
-			xp.auth(ENV['MEGAZORD_EMAIL'],ENV['MEGAZORD_PASSWORD'])
+			xp.auth(ENV['SIMPLE_TOKEN_EMAIL'],ENV['SIMPLE_TOKEN_PASSWORD'])
 			response.headers['Access-Control-Allow-Credentials'] = 'true'
 			response.headers['Access-Control-Allow-Origin'] = '*'
 			response.headers['Access-Control-Max-Age'] = '1728000'
+			puts EXPA.client.email
 			render :json => {:token => xp.get_updated_token.to_s}
 		else
 			response.headers['Access-Control-Allow-Credentials'] = 'true'
 			response.headers['Access-Control-Allow-Origin'] = '*'
 			response.headers['Access-Control-Max-Age'] = '1728000'
+			puts EXPA.client.email
 			render :json => {:token => EXPA.client.get_updated_token.to_s}
 		end
 	end
