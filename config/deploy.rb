c6b2a247673439405ad494b28ae464586b578675
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2017-01-24 10:38:24 -0200

    Linked Files

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :branch,        :test
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/local_env.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
