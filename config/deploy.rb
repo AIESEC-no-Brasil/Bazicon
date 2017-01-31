c1f08e7627c254d629b30306c5b0f7fd299ba43d
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2017-01-31 10:15:31 -0200

    Removes commented configurations

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,6 @@ set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-# Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
@@ -20,7 +19,7 @@ set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_init_active_record, true
 
 ## Defaults:
 # set :scm,           :git
@@ -28,7 +27,6 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :log_level,     :debug
 # set :keep_releases, 5
 
-## Linked Files & Directories (Default None):
 set :linked_files, %w{config/database.yml config/local_env.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
@@ -45,17 +43,6 @@ namespace :puma do
 end
 
 namespace :deploy do
-  # desc "Make sure local git is in sync with remote."
-  # task :check_revision do
-  #   on roles(:app) do
-  #     unless `git rev-parse HEAD` == `git rev-parse origin/master`
-  #       puts "WARNING: HEAD is not the same as origin/master"
-  #       puts "Run `git push` to sync changes."
-  #       exit
-  #     end
-  #   end
-  # end
-
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
