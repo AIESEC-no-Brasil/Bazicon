8fc160f6d4cbf258de2b72f171fc528cebad3211
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2017-02-02 17:10:01 -0200

    Removes bin/ from linked directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :log_level,     :debug
 set :keep_releases, 5
 
 set :linked_files, %w{config/database.yml config/local_env.yml}
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
