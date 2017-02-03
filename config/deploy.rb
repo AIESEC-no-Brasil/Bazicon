137f9ab668c6526f77f1c4aff30cfb5e76458a14
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2017-02-03 11:32:32 -0200

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-set :pty,             true
+# set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
