09ba52e89e7d0c52e9cbc53d8bb557b9bc783cbc
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2017-02-16 10:30:09 -0200

    Update Clockwork sync

--- init_clockwork_scripts.sh
+++ init_clockwork_scripts.sh
@@ -1,2 +1,3 @@
 nohup bundle exec clockwork script/clockwork_expa.rb > clockwork_expa.out &
 nohup bundle exec clockwork script/clockwork_podio.rb > clockwork_podio.out &
+nohup bundle exec clockwork script/clockwork_sync.rb > clockwork_sync.out &
