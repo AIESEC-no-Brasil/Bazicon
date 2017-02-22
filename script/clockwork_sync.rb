9f01563ef0b710ecb958bd3b6e6e14a72740089f
luancorumba <luan@corumba.net>
2017-02-22 12:17:32 -0300

    clockwork_sync without SQS

--- script/clockwork_sync.rb
+++ script/clockwork_sync.rb
@@ -9,29 +9,28 @@ include Clockwork
 # Define the jobs
 handler do |job|
   if job.eql?('oGV')
-    SendJobDataToSqs.call({ "klass" => "Sync", "method" => "check_problematic_applications", "params" => { "programs" => "[1]", "for_filter" => "people" } })
+    Sync.new.check_problematic_applications({ "programs" => "[1]", "for_filter" => "people" })
   elsif job.eql?('oGE')
-    SendJobDataToSqs.call({ "klass" => "Sync", "method" => "check_problematic_applications", "params" => { "programs" => "[5]", "for_filter" => "people" } })
+    Sync.new.check_problematic_applications({ "programs" => "[5]", "for_filter" => "people" })
   elsif job.eql?('oGT')
-    SendJobDataToSqs.call({ "klass" => "Sync", "method" => "check_problematic_applications", "params" => { "programs" => "[2]", "for_filter" => "people" } })
+    Sync.new.check_problematic_applications({ "programs" => "[2]", "for_filter" => "people" })
   elsif job.eql?('iGV')
-    SendJobDataToSqs.call({ "klass" => "Sync", "method" => "check_problematic_applications", "params" => { "programs" => "[1]", "for_filter" => "opportunities" } })
+    Sync.new.check_problematic_applications({ "programs" => "[1]", "for_filter" => "opportunities" })
   elsif job.eql?('iGE')
-    SendJobDataToSqs.call({ "klass" => "Sync", "method" => "check_problematic_applications", "params" => { "programs" => "[5]", "for_filter" => "opportunities" } })
+    Sync.new.check_problematic_applications({ "programs" => "[5]", "for_filter" => "opportunities" })
   elsif job.eql?('iGT')
-    SendJobDataToSqs.call({ "klass" => "Sync", "method" => "check_problematic_applications", "params" => { "programs" => "[2]", "for_filter" => "opportunities" } })
+    Sync.new.check_problematic_applications({ "programs" => "[2]", "for_filter" => "opportunities" })
   elsif job.eql?('opportunities')
-    SendJobDataToSqs.call({ "klass" => "Sync", "method" => "check_problematic_opportunities" })
+    Sync.new.check_problematic_opportunities
   end
   puts "Running EXPA #{job} starting #{Time.now}"
 end
 
 # Define the schedule
-#every(3.day, 'oGE')
-#every(3.day, 'oGT')
-#every(3.day, 'iGV')
-#every(3.day, 'iGE')
-#every(3.day, 'iGT')
-#every(3.day, 'oGV')
-#every(3.day, 'opportunities')
-#every(1.day, 'list all applications and update people', :at => '16:48')
+every(3.day, 'oGE')
+every(3.day, 'oGT')
+every(3.day, 'iGV')
+every(3.day, 'iGE')
+every(3.day, 'iGT')
+every(3.day, 'oGV')
+every(3.day, 'opportunities')
