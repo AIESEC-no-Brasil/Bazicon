430b06aa5e83fc6ee68efb28ef9009ef81a5d9cf
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2017-04-27 14:24:02 -0300

    Fixes typo in deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 # App
 server 'ec2-52-8-184-67.us-west-1.compute.amazonaws.com', port: 22, user: 'ubuntu', roles: [:web, :app, :db]
-server 'ec2-54-215-251-199.us-west-1.compute.amazonaws.com', port: 22, user: 'ubuntu', rolse: [:web, :app, :db]
-server 'ec2-54-183-99-215.us-west-1.compute.amazonaws.com', port: 22, user: 'ubuntu', rolse: [:web, :app, :db]
+server 'ec2-54-215-251-199.us-west-1.compute.amazonaws.com', port: 22, user: 'ubuntu', roles: [:web, :app, :db]
+server 'ec2-54-183-99-215.us-west-1.compute.amazonaws.com', port: 22, user: 'ubuntu', roles: [:web, :app, :db]
 
 # Sync
 server 'ec2-54-183-197-1.us-west-1.compute.amazonaws.com', port: 22, user: 'ubuntu', roles: [:web, :app, :workers, :clock]
