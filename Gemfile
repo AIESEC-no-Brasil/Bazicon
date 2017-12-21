fd301c2bdc5195a54a3ee0abc03037bf18fa6ff1
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2017-12-21 06:52:23 -0200

    postback controller

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'turbolinks'
 gem 'uglifier'
 gem 'whenever', :require => false
 gem 'will_paginate', '~> 3.1.0'
+gem 'pagarme'
 
 group :doc do
   gem 'sdoc', '~> 0.4.0', group: :doc
--- app/controllers/api/v1/pagarme/postback_controller.rb
+++ app/controllers/api/v1/pagarme/postback_controller.rb
@@ -2,8 +2,37 @@ module Api
   module V1
     module Pagarme
       class PostbackController < ApplicationController
+        require 'pagarme'
+        
+        skip_before_action :verify_authenticity_token
+
         def update_status
-          payment = Payment.find_by (id: params[:id])
+          if valid_postback?
+            # Handle your code here
+            # postback payload is in params
+            puts "params ======================== "
+            puts params
+            puts "payment_id #{params[:payment_id]}"
+            puts params[:current_status]
+            puts params[:event]
+            puts params[:fingerprint]
+          else
+            render_invalid_postback_response
+          end
+
+          render json: { params: params }
+        end
+
+        protected
+        
+        def valid_postback?
+          raw_post  = request.raw_post
+          signature = request.headers['HTTP_X_HUB_SIGNATURE']
+          PagarMe::Postback.valid_request_signature?(raw_post, signature)
+        end
+        
+        def render_invalid_postback_response
+          render json: {error: 'invalid postback'}, status: 400
         end
       end
     end
