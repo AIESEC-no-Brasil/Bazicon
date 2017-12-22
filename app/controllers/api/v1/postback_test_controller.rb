f9bacc94085f667e957c57c15d40f3326f099546
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2017-12-22 11:17:34 -0200

    removes unused files

--- app/controllers/api/v1/postback_test_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::PostbackTestController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
-  def status_update
-    render json: { params: params }
-  end
-end
--- spec/controllers/api/v1/postback_test_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Api::V1::PostbackTestController, type: :controller do
-
-end
