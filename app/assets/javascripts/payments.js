469c7593538d2313f8d346ecbd8cfc03b1fe0f40
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2017-12-22 09:29:05 -0200

    resolve conflicts

--- app/assets/javascripts/payments.js
+++ app/assets/javascripts/payments.js
@@ -1,4 +1,5 @@
 //= require forms.js
+//= require pagarme_checkout.js
 
 $().ready(function() {
   $("#payment_value").maskMoney({prefix:'R$ ', allowNegative: true, thousands:'.', decimal:',', affixesStay: false});
