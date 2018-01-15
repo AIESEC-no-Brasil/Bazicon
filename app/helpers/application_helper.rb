95cb779d6fc8036e32b88cf792e86f288fb9c607
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2018-01-15 15:26:58 -0200

    Adds sortable mechanism @rbates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
 
+    def sortable(column, title = nil)
+      title ||= column.titleize
+      css_class = 'table-link'
+      direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+      link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    end
+
     def is_active_controller(controller_name)
         params[:controller] == controller_name ? "active" : nil
     end
